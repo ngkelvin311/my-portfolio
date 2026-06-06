78936e24549988541b9b9c59482b57b539146397
Kelvin Ng <kelvin@Kelvin-Fitness-Passport.local>
2026-06-06 16:44:03 +1000

    chore: block protected paths from search indexing

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,19 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/work/reecetech",
+          "/work/fitness-passport",
+          "/work/dcm",
+          "/login",
+          "/api/",
+        ],
+      },
+    ],
     sitemap: "https://kelvinux.com/sitemap.xml",
   };
 }
